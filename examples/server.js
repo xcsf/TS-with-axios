c938f54dff63c5a578b944228dc37b8cf45f54a9
yinchengyu <yinchy@szmedi.com.cn>
2019-07-16 17:08:05 +0800

    add router /simple/get

--- examples/server.js
+++ examples/server.js
@@ -22,7 +22,15 @@ app.use(express.static(__dirname))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+const router = express.Router()
 
+router.get('/simple/get', function(req, res) {
+  res.json({
+    msg: `hello world`
+  })
+})
+
+app.use(router)
 const port = process.env.PORT || 8080
 module.exports = app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
