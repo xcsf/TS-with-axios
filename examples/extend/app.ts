00ac94c2966c6e1d7aa39938e346a5e45aefae92
yinchengyu <yinchy@szmedi.com.cn>
2019-07-31 14:55:03 +0800

    feat(type/index.ts): add <T = any>
    add <T = any>

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -36,3 +36,44 @@ axios.post('/extend/post', { msg: 'post' })
 axios.put('/extend/put', { msg: 'put' })
 
 axios.patch('/extend/patch', { msg: 'patch' })
+
+// 请求接口数据
+export interface ResponseData<T = any> {
+    /**
+     * 状态码
+     * @type { number }
+     */
+    code: number
+  
+    /**
+     * 数据
+     * @type { T }
+     */
+    result: T
+  
+    /**
+     * 消息
+     * @type { string }
+     */
+    message: string
+  }
+
+  function getUser<T>() {
+    return axios.get<ResponseData<T>>('/extend/user')
+      .then(res => res.data)
+      .catch(err => console.error(err))
+  }
+  interface User {
+    name: string
+    age: number
+  }
+  async function test() {
+    // user 被推断出为
+    // {
+    //  code: number,
+    //  result: { name: string, age: number },
+    //  message: string
+    // }
+    const user = await getUser<User>()
+  }
+  test()
