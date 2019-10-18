61650dd6c1491b652d6b8eb2c26cba5e9fddcfc0
yinchengyu <jiaowofeizi@126.com>
2019-10-18 17:47:24 +0800

    feat(default dispatchrequest transform): transform config
    transform config

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,4 +1,6 @@
 import { AxiosRequestConfig } from './types'
+import { transformResponse, transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 
 const defaults: AxiosRequestConfig = {
     method: 'get',
@@ -9,7 +11,19 @@ const defaults: AxiosRequestConfig = {
         common: {
             Accept: 'application/json, text/plain, */*'
         }
-    }
+    },
+
+    transformResponse: [
+        function(data: any): any {
+            return transformResponse(data)
+        }
+    ],
+    transformRequest: [
+        function(data: any, headers: any): any {
+            processHeaders(headers, data)
+            return transformRequest(data)
+        }
+    ]
 }
 
 const methodsNoData = ['delete', 'get', 'head', 'options']
@@ -17,7 +31,6 @@ const methodsNoData = ['delete', 'get', 'head', 'options']
 methodsNoData.forEach(method => {
     defaults.headers[method] = {}
 })
-
 const methodsWithData = ['post', 'put', 'patch']
 
 methodsWithData.forEach(method => {
