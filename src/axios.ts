9bd879d9c085ceba4e8f3fc3d9f146cb3f9b21be
yinchengyu <jiaowofeizi@126.com>
2019-10-18 15:21:42 +0800

    style(none): none
    none

--- src/axios.ts
+++ src/axios.ts
@@ -4,10 +4,10 @@ import { extend } from './helpers/util'
 import defaults from './defaults'
 
 function createInstance(config: AxiosRequestConfig): AxiosInstance {
-  const context = new Axios(config)
-  const request = Axios.prototype.request.bind(context)
-  extend(request, context)
-  return request as AxiosInstance
+    const context = new Axios(config)
+    const request = Axios.prototype.request.bind(context)
+    extend(request, context)
+    return request as AxiosInstance
 }
 const axios = createInstance(defaults)
 export default axios
